abed9628c62a94da1b93477d3f150a8237a2f48b
Andreas Schmidt <15648021+iamasc8277@users.noreply.github.com>
2026-01-13 10:55:11 +0100

    update fish config

--- config.fish
+++ config.fish
@@ -12,19 +12,6 @@ if status is-interactive
 
     set -g fish_prompt_pwd_dir_length 0
 
-    set -g __fish_git_prompt_showdirtystate 1
-    set -g __fish_git_prompt_showuntrackedfiles 1
-    set -g __fish_git_prompt_showstashstate 1
-    set -g __fish_git_prompt_showupstream "verbose"
-    set -g __fish_git_prompt_showcolorhints 1
-    set -g __fish_git_prompt_use_informative_chars 1
-
-    set -g __fish_git_prompt_char_dirtystate '✗'
-    set -g __fish_git_prompt_char_untrackedfiles '?'
-    set -g __fish_git_prompt_char_upstream_ahead '↑'
-    set -g __fish_git_prompt_char_upstream_behind '↓'
-    set -g __fish_git_prompt_char_stateseparator ' '
-
     function fish_prompt
         set -l last_status $status
         if not set -q __fish_prompt_initialized
@@ -50,10 +37,10 @@ if status is-interactive
         set_color cyan
         echo -n (prompt_pwd)
         set_color yellow
-        fish_git_prompt
+        fish_vcs_prompt
         echo "" 
         set_color normal
-        echo -n "╰> "
+        echo -n "~> "
         set_color normal
     end
 end
